4f3f00a4dbdfeb0ccf539078bdcff8c259dd44e0
ly9999y <478125686@qq.com>
2020-12-04 00:39:35 +0800

    结束

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import $ from 'jquery'
+import './css/1.css'
+import './css/1.less'
+import './css/1.scss'
+
 
 $(function(){
     $("li:odd").css('backgroundColor', 'pink');
     $("li:even").css('backgroundColor', 'lightblue');
+})
+
+// ----------------------------
+import Vue from 'vue'
+import App from './components/App.vue'
+
+const vm = new Vue({
+    el: '#app',
+    render: h=>h(App)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path=require('path');
 //导入包
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+const vuePlugin = new VueLoaderPlugin();
+
 //创建对象
 const htmlPlugin = new HtmlWebpackPlugin({
     //设置生成预览页面的模板文件
@@ -26,5 +31,40 @@ module.exports={
         inline: true,
         progress:true
     },
-    plugins:[ htmlPlugin ]
+    plugins:[ htmlPlugin, vuePlugin ],
+    module: {
+        rules:[
+            {
+                //test设置需要匹配的文件类型，支持正则
+                test:/\.css$/,
+                //use表示该文件类型需要调用的loader
+                use:['style-loader','css-loader','postcss-loader']
+            },
+            {
+                test:/\.less$/,
+                use:['style-loader','css-loader','less-loader']
+            },
+            {
+                test:/\.scss$/,
+                use:['style-loader','css-loader','sass-loader']
+            },
+            {
+                test:/\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/,
+                //limit用来设置字节数，只有小于limit值的图片，才会转换
+                //为base64图片
+                use:"url-loader?limit=200000000"
+            },
+            {
+                test:/\.js$/,
+                use:"babel-loader",
+                //exclude为排除项，意思是不要处理node_modules中的js文件
+                exclude:/node_modules/
+            },
+            { 
+                test:/\.vue$/,
+                loader:"vue-loader",
+                
+            }
+        ]
+    }
 }
